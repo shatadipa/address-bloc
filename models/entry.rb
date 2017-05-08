24cd7203fedae3ec1a4c321cc5a1252f79a04540
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2017-05-07 23:15:18 -0700

    Chapter 5 Checkpoint

---
+++ models/entry.rb
@@ -0,0 +1,26 @@
+class Entry
+
+  # These must be accessors since we mutate them
+  attr_accessor :name, :phone_number, :email
+
+  def initialize(name, phone_number, email)
+    @name = name
+    @phone_number = phone_number
+    @email = email
+  end
+
+  def to_s
+    return "Name: #{@name}\nPhone Number: #{@phone_number}\nEmail: #{@email}"
+  end
+=begin
+  def name
+    return @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+=end
+
+
+end
