45b3bf42a921a30758b467576ebabca77b398459
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2017-05-19 01:20:57 -0700

    Chapter 8 Checkpoint

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -83,7 +83,10 @@ class MenuController
     case selection
       when 'n'
       when 'd'
+        delete_entry(entry)
       when 'e'
+        edit_entry(entry)
+        entry_submenu(entry)
       when 'm'
         system 'clear'
         main_menu
@@ -112,9 +115,89 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+    # #10
+    match = address_book.binary_search(name)
+    system "clear"
+    # #11
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
+  def search_submenu(entry)
+    # #12
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    # #13
+    selection = gets.chomp
+
+    # #14
+    case selection
+      when "d"
+        system "clear"
+        delete_entry(entry)
+        main_menu
+      when "e"
+        edit_entry(entry)
+        system "clear"
+        main_menu
+      when "m"
+        system "clear"
+        main_menu
+      else
+        system "clear"
+        puts "#{selection} is not a valid input"
+        puts entry.to_s
+        search_submenu(entry)
+    end
+  end
+
+  
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+    puts "Updated entry:"
+    puts entry
+  end
+
+
   def read_csv
+    print "Enter csv file to import:"
+    file_name=gets.chomp()
+
+    if file_name.empty?
+      system "clear"
+      puts "No csv file read"
+      main_menu
+    end
+    begin
+      entry_count=@address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count}  new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
 end
